a8e402337d8078184efb26758e0e5992a6eb767c
cbrownaz24 <connorbrown059@gmail.com>
2026-02-14 19:02:53 -0500

    Modified __init__.py -> EfficientZero to have do_dynamics() and recurrrent_inference() to manage a history buffer of the past seq_len # of states alongside fixed action history. Changed initial_inference() to create the initial state history, zero-padded

--- ez/agents/models/__init__.py
+++ ez/agents/models/__init__.py
@@ -52,6 +52,7 @@ class EfficientZero(nn.Module):
         self.state_norm = kwargs.get('state_norm')
         self.value_prefix = kwargs.get('value_prefix')
         self.v_num = config.train.v_num
+        self.seq_len = config.model.seq_len
 
     def do_representation(self, obs):
         state = self.representation_model(obs)
@@ -60,8 +61,12 @@ class EfficientZero(nn.Module):
 
         return state
 
-    def do_dynamics(self, state, action):
-        next_state = self.dynamics_model(state, action)
+    def do_dynamics(self, state_history, action_history):
+        assert state_history.shape[0] == action_history.shape[0], f"Batch size of state history {state_history.shape[0]} does not match that of action history {action_history.shape[0]}"
+        assert state_history.shape[1] == self.seq_len, f"Sequence length of state history {state_history.shape[1]} does not match the expected {self.seq_len}"
+        assert action_history.shape[1] == self.seq_len, f"Sequence length of action history {action_history.shape[1]} does not match the expected {self.seq_len}"
+
+        next_state = self.dynamics_model(state_history, action_history)
         if self.state_norm:
             next_state = normalize_state(next_state)
 
@@ -93,10 +98,12 @@ class EfficientZero(nn.Module):
 
     def initial_inference(self, obs, training=False):
         state = self.do_representation(obs)
+        state_history = torch.zeros(state.shape[0], self.seq_len - 1, *state.shape[1:], device=state.device, dtype=state.dtype)
+        state_history = torch.cat([state_history, state.unsqueeze(1)], dim=1)
         values, policy = self.do_value_policy_prediction(state)
 
         if training:
-            return state, values, policy
+            return state, state_history, values, policy
 
         if self.v_num > 2:
             values = values[np.random.choice(self.v_num, 2, replace=False)]
@@ -108,15 +115,18 @@ class EfficientZero(nn.Module):
         if self.config.env.env in ['DMC', 'Gym']:
             output_values = output_values.clip(0, 1e5)
 
-        return state, output_values, policy
+        return state, state_history, output_values, policy
 
 
-    def recurrent_inference(self, state, action, reward_hidden, training=False):
-        next_state = self.do_dynamics(state, action)
+    def recurrent_inference(self, history, reward_hidden, training=False):
+        # history: tuple of (state_history, action_history) where we have s[i]->a[i]->s[i+1]
+        state_history, action_history = history
+        next_state = self.do_dynamics(state_history, action_history)
+        new_state_history = torch.cat([state_history[:, 1:, :, :, :], next_state.unsqueeze(1)], dim=1)
         value_prefix, reward_hidden = self.do_reward_prediction(next_state, reward_hidden)
         values, policy = self.do_value_policy_prediction(next_state)
         if training:
-            return next_state, value_prefix, values, policy, reward_hidden
+            return next_state, new_state_history, value_prefix, values, policy, reward_hidden
 
         if self.v_num > 2:
             values = values[np.random.choice(self.v_num, 2, replace=False)]
@@ -132,7 +142,7 @@ class EfficientZero(nn.Module):
             value_prefix = symexp(value_prefix)
         else:
             value_prefix = DiscreteSupport.vector_to_scalar(value_prefix, **self.config.model.reward_support)
-        return next_state, value_prefix, output_values, policy, reward_hidden
+        return next_state, new_state_history, value_prefix, output_values, policy, reward_hidden
 
     def get_weights(self, part='none'):
         if part == 'reward':
